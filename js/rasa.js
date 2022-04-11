4c09a558525494a5a7e4abbe14d11bc6165fd5d6
phcsouza <phcsouza@unifesp.br>
2022-04-11 05:39:19 -0300

    codigo html atualizado

---
+++ js/rasa.js
@@ -0,0 +1,40 @@
+!(function () {
+  let e = document.createElement("script"),
+      t = document.head || document.getElementsByTagName("head")[0];
+  (e.src =
+      "https://cdn.jsdelivr.net/npm/rasa-webchat@1.x.x/lib/index.js"),
+      // Replace 1.x.x with the version that you want
+      (e.async = !0),
+      (e.onload = () => {
+          sessionStorage.clear();
+          window.WebChat.default(
+              {
+                  customData: { language: "en" },
+                  socketUrl: "rasa-server-phcsouza.cloud.okteto.net",
+                  // add other props here
+                  initPayload: "/boas_vindas",
+                  title: 'Chatbot Retorno Seguro',
+                  inputTextFieldHint: 'Digite sua mensagem...',
+                  mainColor: '#014341',
+                  conversationBackgroundColor: '#FFFFFF',
+                  userTextColor: '',
+                  userBackgroundColor: '#6E7F6D',
+                  assistTextColor: '',
+                  assistBackgoundColor: '#DCDCDC',
+                  params: {
+                      storage: 'session'
+                  },
+                  // autoClearCache: false,
+                  // displayUnreadCount: false,
+                  // onWidgetEvent: {
+                  //     onChatOpen: () => { },
+                  //     onChatClose: () => { },
+                  //     onChatVisible: () => { },
+                  //     onChatHidden: () => { }
+                  // },
+              },
+              null
+          );
+      }),
+      t.insertBefore(e, t.firstChild);
+})();
